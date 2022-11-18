413072be55936127a6ef1fdc7486bb60824f8f93
Logvynskyy <shimon511bm@gmail.com>
2022-11-18 20:32:40 +0100

    added enough endpoints and methods: followed requirements of the lab

--- api/hello.ts
+++ api/hello.ts
@@ -2,14 +2,25 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Router } from '../src/router';
 
 const router = new Router('api');
-const hello = 'Hello World!123';
 
 router.get('/hello', (req: VercelRequest, res: VercelResponse) => {
-  res.status(200).send({ message: hello });
+  res.status(200).send({ message: 'Hello from get method' });
 });
 
-router.delete('/delete', (req, res) => {
+router.post('/hello', (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).send({ message: 'Hello from post method' });
+});
+
+router.delete('/hello', (req, res) => {
   res.send({ message: 'Hello from delete method' });
+});
+
+router.put('/second', (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).send({ message: 'Hello from 2nd endpoint and 4th method' });
+});
+
+router.get('/third', (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).send({ message: 'Hello from 3nd endpoint' });
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
